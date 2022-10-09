8f2acf407b4ada726ba78e0939d7bdb7775cafbb
Brian Reece <bdreece@mtu.edu>
2022-10-09 01:30:09 -0400

    Updated server

--- apps/server/src/api/ping.ts
+++ apps/server/src/api/ping.ts
@@ -1,10 +1,12 @@
 import type { Handler } from 'types/handler';
 
-const get: Handler<string> = async (_, res) =>
+const get: Handler<string> = async (_, res) => {
+  console.log('Ping!');
   res.json({
     success: true,
     data: 'Pong!',
   });
+};
 
 export default {
   get,
--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import compression from 'compression';
 import morgan from 'morgan';
 
@@ -8,6 +9,7 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.json());
+app.use(cors({ origin: '*' }));
 app.use(compression());
 app.use(morgan('dev'));
 
